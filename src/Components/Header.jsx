dbd4f4b6f65142b487bbd351758837f90da50f22
Bastian Gutiérrez <bas.gutierrez@gmail.com>
2023-09-12 13:38:13 -0300

    Formato header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
+import "./Header.css"
 
+import { IconCamera } from "@tabler/icons-react";
 export const Header = () => {
   return (
     <>
-      <header>NATURAL PIC</header>
+      <header>
+        <h1> <IconCamera /> Natural<span>Pic</span></h1>
       <nav>
         <Link to="/">Home</Link>
         <Link to="/Favoritos">Favoritos</Link>
       </nav>
+      </header>
     </>
   );
 };
